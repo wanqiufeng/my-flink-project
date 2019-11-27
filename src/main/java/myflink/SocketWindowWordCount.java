dabc44b26d7296f300033942b87f839ccfd75b68
西北偏北 <wanqiufeng@outlook.com>
2019-11-27 17:38:19 +0800

    test

--- src/main/java/myflink/SocketWindowWordCount.java
+++ src/main/java/myflink/SocketWindowWordCount.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,42 +15,103 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package myflink;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 
+/**
+ * Implements a streaming windowed version of the "WordCount" program.
+ *
+ * <p>This program connects to a server socket and reads strings from the socket.
+ * The easiest way to try this out is to open a text server (at port 12345)
+ * using the <i>netcat</i> tool via
+ * <pre>
+ * nc -l 12345 on Linux or nc -l -p 12345 on Windows
+ * </pre>
+ * and run this example with the hostname and the port as arguments.
+ */
+@SuppressWarnings("serial")
 public class SocketWindowWordCount {
 
 	public static void main(String[] args) throws Exception {
 
-		// 创建 execution environment
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		// the host and the port to connect to
+		final String hostname;
+		final int port;
+		try {
+			final ParameterTool params = ParameterTool.fromArgs(args);
+			hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+			port = params.getInt("port");
+		} catch (Exception e) {
+			System.err.println("No port specified. Please run 'SocketWindowWordCount " +
+				"--hostname <hostname> --port <port>', where hostname (localhost by default) " +
+				"and port is the address of the text server");
+			System.err.println("To start a simple text server, run 'netcat -l <port>' and " +
+				"type the input text into the command line");
+			return;
+		}
 
-		// 通过连接 socket 获取输入数据，这里连接到本地9000端口，如果9000端口已被占用，请换一个端口
-		DataStream<String> text = env.socketTextStream("localhost", 9000, "\n");
+		// get the execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		// 解析数据，按 word 分组，开窗，聚合
-		DataStream<Tuple2<String, Integer>> windowCounts = text
-				.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+		// get input data by connecting to the socket
+		DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+
+		// parse the data, group it, window it, and aggregate the counts
+		DataStream<WordWithCount> windowCounts = text
+
+				.flatMap(new FlatMapFunction<String, WordWithCount>() {
 					@Override
-					public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+					public void flatMap(String value, Collector<WordWithCount> out) {
 						for (String word : value.split("\\s")) {
-							out.collect(Tuple2.of(word, 1));
+							out.collect(new WordWithCount(word, 1L));
 						}
 					}
 				})
-				.keyBy(0)
-				.timeWindow(Time.seconds(5))
-				.sum(1);
 
-		// 将结果打印到控制台，注意这里使用的是单线程打印，而非多线程
+				.keyBy("word")
+				.timeWindow(Time.seconds(5))
+
+				.reduce(new ReduceFunction<WordWithCount>() {
+					@Override
+					public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+						return new WordWithCount(a.word, a.count + b.count);
+					}
+				});
+
+		// print the results with a single thread, rather than in parallel
 		windowCounts.print().setParallelism(1);
 
 		env.execute("Socket Window WordCount");
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Data type for words with count.
+	 */
+	public static class WordWithCount {
+
+		public String word;
+		public long count;
+
+		public WordWithCount() {}
+
+		public WordWithCount(String word, long count) {
+			this.word = word;
+			this.count = count;
+		}
+
+		@Override
+		public String toString() {
+			return word + " : " + count;
+		}
 	}
 }
